d640158f6953e087a84cf229e1ae62cbbead752c
Vladimir Belich <belichvladimir@mail.ru>
2022-02-14 11:24:12 +0300

    Hw3_1

--- homework3.sql
+++ homework3.sql
@@ -1,31 +1,34 @@
-create table if not exists tracks (
-id serial primary key,
-name varchar(30) not null,
-duration time not null);
-
-create table if not exists artists (
-id serial primary key,
-name varchar(50) unique not null,
-genre_id integer not null,
-alboms_id integer not null);
-
 create table if not exists genres (
 id serial primary key,
-name varchar unique not null,
-artist_id integer references artists(id));
+name varchar unique not null);
+
+create table if not exists artists (
+id integer primary key,
+name varchar(30) unique not null);
+
+create table if not exists artists_genre(
+id serial primary key,
+artistId integer not null references artists(Id),
+genreId integer not null references genres(id));
 
 create table if not exists alboms (
-id serial primary key references tracks(id),
+id serial primary key,
 name varchar(50) not null,
-year_albom date not null,
-artist_id integer not null references artists(id));
+year_albom integer not null);
+
+create table if not exists ArtistAlbom(
+id serial primary key,
+artist_id integer references artists(id),
+albom_id integer references alboms(id));
+
+create table if not exists tracks (
+id integer primary key,
+name varchar(40) not null,
+duration numeric not null);
 
 create table if not exists collections (
 id serial primary key,
 name varchar(30) unique not null,
-year varchar(4) not null,
+year_collection integer not null,
 track_id integer not null references tracks(id),
 albom_id integer not null references alboms(id));
-
-alter table artists add foreign key(genre_id) references genres(id);
-
